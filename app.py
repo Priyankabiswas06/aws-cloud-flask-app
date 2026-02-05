debf93bc44799992b31c919d6c2fb4c37f9a766d
Priyanka Biswas <124877145+Priyankabiswas06@users.noreply.github.com>
2026-02-05 12:31:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Cloud Project Deployed Successfully on AWS!"
+    return """
+    <html>
+      <head>
+        <title>AWS Cloud Project</title>
+        <style>
+          body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+            margin-top: 80px;
+          }
+        </style>
+      </head>
+      <body>
+        <h1>Cloud Project Deployed Successfully on AWS 🚀</h1>
+        <p>Flask + Gunicorn + Nginx</p>
+      </body>
+    </html>
+    """
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run()
